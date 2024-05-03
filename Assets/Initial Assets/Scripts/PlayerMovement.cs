fa97d4665227e3fb316b14f3bdc6ede04ade1ec2
MZRSE <maddyb@swbell.net>
2024-05-03 15:14:41 -0500

    Finished FPS movement
    god don't even... talk to me. don't look at me, don't comprehend me, I know, alright, just leave me alone. Yea, what my buds said really got to me... i know they didn't mean to be hurtful, they told it like it is and they're right. *but,* they also made the paralysis worse, and made me beat myself up even harder. all i wanted was a bit of sympathy, and instead i get the backburn. can i burn thru a semester's worth of reading and journaling about a novel in a day? i dont doubt my ability to. do i have the motivation to? buddi i don't have the motivation to do ANYTHING. much less finish a ten minute tutorial.
    Well, the *good* news is It's DONE Now, and you know, what, i deserve a good back patting, but im not gettin it. Next up, figure out where i left off on this spray tutorial, and finish *that* one up.

--- Assets/Initial Assets/Scripts/PlayerMovement.cs
+++ Assets/Initial Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,18 @@ public class PlayerMovement : MonoBehaviour
     public float _moveSpeed;
     public float _groundDrag;
 
+    public float _jumpForce;
+    public float _jumpCooldown;
+    public float _airMult;
+    public bool _jumpReady;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
     [Header("Ground Check")]
     public float _playerHeight;
     public LayerMask _identifyGround;
-    bool _grounded;
+    public bool _grounded;
 
     public Transform _orient;
 
@@ -25,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        _jumpReady = true;
     }
 
     private void Update()
@@ -50,13 +59,23 @@ public class PlayerMovement : MonoBehaviour
     {
         _horiInput = Input.GetAxisRaw("Horizontal");
         _vertiInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && _jumpReady && _grounded)
+        {
+            _jumpReady = false;
+            Jump();
+            Invoke(nameof(ResetJump), _jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         _moveDirection = _orient.forward * _vertiInput + _orient.right * _horiInput;
 
-        rb.AddForce(_moveDirection.normalized * _moveSpeed * 10f, ForceMode.Force);
+        if (_grounded)
+            rb.AddForce(_moveDirection.normalized * _moveSpeed * 10f, ForceMode.Force);
+        else if (!_grounded)
+            rb.AddForce(_moveDirection.normalized * _moveSpeed * 10f * _airMult, ForceMode.Force);
     }
 
     private void SpeedControl()
@@ -68,5 +87,16 @@ public class PlayerMovement : MonoBehaviour
             Vector3 _limitVelocity = _flatVelocity.normalized * _moveSpeed;
             rb.velocity = new Vector3(_limitVelocity.x, rb.velocity.y, _limitVelocity.z);
         }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        _jumpReady = true;
     }
 }
